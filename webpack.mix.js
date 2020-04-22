f7ac32488c6c5908974d27925bf30c6bbd359b3f
Armando <armandovanoeffelen@gmail.com>
2020-04-22 16:44:01 +0200

    made some changes hopefully they work

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,5 +13,7 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/dashboard/_ds.scss','public/css')
     .sass('resources/sass/app.scss', 'public/css').version();
+mix.js('resources/js/enable-push.js','public/js');
+mix.js('resources/js/sw.js','public/js');
 
 
